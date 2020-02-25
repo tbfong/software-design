3297bc5f73b902c757f991419289e4aed9f5a33b
tbfong <Tyler.Fong@kellogg.northwestern.edu>
2020-02-25 18:43:30 +0000

    Session1

--- app.rb
+++ app.rb
@@ -14,3 +14,19 @@ after { puts; }                                                             #
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
+
+get "/" do 
+puts events_table.all 
+@events = events_table.all
+view "events"
+end 
+
+get "/events/:id" do 
+   @event = events_table.where(id: params[:id]).first
+   view "event"
+end 
+
+get "events/:id/rsvps/new" do 
+   @event = events_table.where(id: params[:id]).first
+   view "new_rsvp"
+end
